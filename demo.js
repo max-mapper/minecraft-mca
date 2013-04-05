c84ff32bb5cf27471ff041854ea7938d244d8690
Max Ogden <max@maxogden.com>
2013-04-04 23:51:38 -0700

    tweak demo

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@ var textures = "http://commondatastorage.googleapis.com/voxeltextures/"
 var materials = [['grass', 'grass', 'grass'], 'stone', 'lava2', 'iron', 'gravel', 'coal', 'dirt', 'water2', 'wood', 'cobble', 'leaves', 'log', 'mossy']
 // var worldOrigin = [0, 67, 320]
 // var worldOrigin = [-61, 169, 329]
-var worldOrigin = [225, 80, 0]
+var worldOrigin = [225, 80, 50]
 var regionFile = 'r.-1.0.mca'
 
 var render = require('./')
@@ -30,7 +30,7 @@ game.controls.target().avatar.position.copy({x: wc.x, y: wc.y, z: wc.z})
 binaryXHR(regionFile, function(err, data) {
   if (err) return console.error(err)
   var region = mcRegion(data)
-  var opts = {x: playerPos.x, y: playerPos.y, z: playerPos.z, size: game.chunkDistance, ymin: 0, onVoxel: function(x, y, z, type, offsetX, offsetZ) {
+  var opts = {x: worldOrigin[0], y: worldOrigin[1], z: worldOrigin[2], size: game.chunkDistance, ymin: 0, onVoxel: function(x, y, z, type, offsetX, offsetZ) {
     game.setBlock([(offsetX * 16) + x, y, (offsetZ * 16) + z], type)
   }}
   var view = render(region, opts)
